d0d1d6c6c899e3d9c77559e34dd4fe6d472611f9
shohagroy <pkshohag240@gmail.com>
2022-10-25 11:01:56 +0600

    course category added

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const cors = require("cors");
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+const courses = require("./data/course.json");
 
 app.get("/", (req, res) => {
   res.send("News API Running");
 });
 
+app.get("/course", (req, res) => {
+  res.send(courses);
+});
+
 app.listen(port, () => {
-  console.log("Dragon News Server running on port", port);
+  console.log("Ghuri Learning Server running on port:", port);
 });
