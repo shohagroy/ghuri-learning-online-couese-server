7634ab119dfdb1b04b3804640226aeeed30c287d
shohagroy <pkshohag240@gmail.com>
2022-10-26 03:08:23 +0600

    json file update

--- index.js
+++ index.js
@@ -8,13 +8,20 @@ const port = process.env.PORT || 5000;
 
 const category = require("./data/category.json");
 const courses = require("./data/course.json");
+const courseDetails = require("./data/courseDetails.json");
 
 app.get("/", (req, res) => {
   res.send(category);
 });
 
-app.get("/course", (req, res) => {
+app.get("/courses", (req, res) => {
   res.send(courses);
+});
+
+app.get("/courses/details/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courseDetails.find((search) => search.id === id);
+  res.send(course);
 });
 
 app.listen(port, () => {
